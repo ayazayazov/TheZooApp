700b73877bb52bc5ca5e6f17fe75af2882970e1c
ayazayazov <ayazayazov00@gmail.com>
2024-06-09 23:17:54 +0400

    zoo name grid demo

--- The Zoo/Controllers/InfoController.swift
+++ The Zoo/Controllers/InfoController.swift
@@ -9,16 +9,20 @@ import UIKit
 
 class InfoController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
+    var isGridView = false
     let items = ["abc","qwe","rty","uio","plk","jhg","fds","azx","cvb"]
+    
     
     @IBOutlet weak var searchBar: UISearchBar!
     
-
+    @IBOutlet weak var gridButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         self.searchBar.searchTextField.backgroundColor = .white
+        self.searchBar.searchTextField.textColor = .black
         self.tabBarController?.navigationItem.hidesBackButton = true
         self.navigationController?.navigationBar.isHidden = true
     }
@@ -37,9 +41,28 @@ class InfoController: UIViewController, UICollectionViewDataSource, UICollection
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        .init(width: collectionView.frame.width, height: 80)
+        
+        if isGridView {
+            return .init(width: collectionView.frame.width/2-10, height: 80)
+        } else {
+            return .init(width: collectionView.frame.width, height: 80)
+        }
+        
         
     }
     
-
+    @IBAction func gridButtonTapped(_ sender: UIButton) {
+        
+    
+        
+        if isGridView {
+           
+            gridButton.setImage(UIImage(systemName: "square.grid.2x2.fill"), for: .normal)
+        } else {
+            gridButton.setImage(UIImage(systemName: "list.bullet"), for: .normal)
+        }
+        isGridView = !isGridView
+    }
+    
+    
 }
